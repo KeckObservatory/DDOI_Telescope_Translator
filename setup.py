f5018b2a7904bdce892215e9a771fbeedc59be78
= <=>
2022-06-29 12:03:14 -1000

    added setup for package

---
+++ setup.py
@@ -0,0 +1,35 @@
+
+from setuptools import setup, find_packages
+
+# Get some values from the setup.cfg
+
+NAME = 'DDOI_Telescope_Translator'
+VERSION = '0.1.1'
+RELEASE = 'dev' not in VERSION
+AUTHOR = "Lucas Fuhrman"
+AUTHOR_EMAIL = "lfuhrman@keck.hawaii.edu"
+LICENSE = "3-clause BSD"
+DESCRIPTION = "The Base Telescope Translator Module"
+
+scripts = []
+
+# Define entry points for command-line scripts
+entry_points = {
+    'console_scripts': [
+        "translator = ddoitranslatormodule.cli_interface:main",
+    ]
+    }
+
+setup(name=NAME,
+      provides=NAME,
+      version=VERSION,
+      license=LICENSE,
+      description=DESCRIPTION,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      packages=find_packages(),
+      scripts=scripts,
+      entry_points=entry_points,
+      install_requires=[],
+      python_requires=">=3.6"
+      )
