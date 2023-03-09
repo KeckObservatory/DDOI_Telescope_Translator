ba2957ecdd5a7c466dbb1141100976d41eb55e25
Max Brodheim <max.brodheim@gmail.com>
2023-03-09 11:21:52 -1000

    add ddoi_script_function stubs

---
+++ telescopetranslator/ddoi_script_functions/waitfor_slew.py
@@ -0,0 +1,19 @@
+
+from telescopetranslator.BaseTelescope import TelescopeBase
+
+class begin_slew(TelescopeBase):
+
+    '''
+    '''
+    @classmethod
+    def pre_condition(cls, args, logger, cfg):
+        return True
+
+    @classmethod
+    def perform(cls, args, logger, cfg):
+        logger.info("I would wait for slew, if I knew how!")
+
+
+    @classmethod
+    def post_condition(cls, args, logger, cfg):
+        return True
